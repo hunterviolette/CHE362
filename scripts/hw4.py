40c74a5f050dbdbecc7d3357cb7cabff74579c09
hunterviolette <hunter@maplevalleypharms.com>
2023-06-07 15:21:11 -0400

    hw4 graphing

--- scripts/hw4.py
+++ scripts/hw4.py
@@ -4,7 +4,9 @@ from sympy import Eq, symbols
 from sympy.physics.units import cm, atm, m, molar_gas_constant, \
     kelvin, second, mol, gram, kilogram, mmHg, mol, hour
 from numpy import arange
+from numpy.random import random
 
+import plotly.graph_objects as go
 import plotly_express as px
 import pandas as pd
 
@@ -100,24 +102,94 @@ class HW4(CHE362):
 
   @staticmethod
   def Two():
+    print('==== Problem 2 ====')
     kmol = 10**3 * mol
 
     # cocurrent absorber 
-    y0, y1 = .02, .001 # [2%, 0.1%] MeOH
-    vp, p = 1 * atm, 2 * atm
+    y0, y1, x0 = .02, .001, 0 # [2%, 0.1%] MeOH
+    vp, p = q(126, 'mmHg').to('atm').magnitude * atm, 1 * atm
     v = 100 * kmol / hour # Total flow rate of extract stream
-    vS = v * (1 - y0)
-    print(vS)
+    Vs, gamma = v * (1 - y0), 1.725
 
-    df = pd.DataFrame()
-    for x in arange(0, 1, .01):
-      y = float((vp / p) * x)
-      capX, capY = HW4.CapXY(x, f'x_{x}'), HW4.CapXY(y, f'y_{x}')
-      df = pd.concat([df, pd.DataFrame({"x":[x], "y":[y], "X":[capX], "Y":[capY]}
-                                    )], ignore_index=True)
+    x1 = HW4.Solve_EquilibRelationship(
+              vp, p, symbols('x1'), y1, symbols('x1'), gamma
+            )
+    
+    X1, X2 = HW4.CapXY(x0), HW4.CapXY(x1)
+    Y1, Y2 = HW4.CapXY(y0), HW4.CapXY(y1)
+
+    print('==== Part c ====')
+    Ls = HW4.Solve_MaterialBal_Streams('cocurrent', 
+                                  X1, X2, Y1, Y2,
+                                  Ls = symbols('Ls'), 
+                                  Vs = Vs, 
+                                  solveFor = symbols('Ls')
+                                )
+    
+    line1 = pd.DataFrame({"X": [X1, X2], "Y":[Y1, Y2]}).astype(float)
+
+
+    print('==== Part d ====')
+    Xn_2Ls = HW4.Solve_MaterialBal_Streams('cocurrent', 
+                                  X1 = HW4.CapXY(x0), 
+                                  X2 = symbols('Xn'), 
+                                  Y1 = HW4.CapXY(y0), 
+                                  Y2 = HW4.CapXY(y1), 
+                                  Ls = Ls*2, 
+                                  Vs = Vs, 
+                                  solveFor = symbols('Xn'),
+                                )
+    line2 = pd.DataFrame({"X": [X1, Xn_2Ls], "Y":[Y1, Y2]}).astype(float)
+    
+    print('==== Part e ====')
+    Xn_1_4Ls = HW4.Solve_MaterialBal_Streams('cocurrent', 
+                                  X1 = HW4.CapXY(x0), 
+                                  X2 = symbols('Xn'), 
+                                  Y1 = HW4.CapXY(y0), 
+                                  Y2 = HW4.CapXY(y1), 
+                                  Ls = Ls*1.4, 
+                                  Vs = Vs, 
+                                  solveFor = symbols('Xn'),
+                                )
+    line3 = pd.DataFrame({"X": [X1, Xn_1_4Ls], "Y":[Y1, Y2]}).astype(float)
+
+    
+    def Graph(plot: bool = False, lines: list = []):
       
-    print(df)
-    px.line(data_frame=df, x='X', y='Y').show()
+      df = pd.DataFrame()
+      for x in arange(0, .5, .001):
+        y = float(((vp * gamma) / p) * x)
+        capX, capY = HW4.CapXY(x, f'x_{x}'), HW4.CapXY(y, f'y_{x}')
+        df = pd.concat([df, pd.DataFrame({"x":[x], "y":[y], "X":[capX], "Y":[capY]}
+                                      )], ignore_index=True)
+        
+      print('==== Plot data ====')
+      df = df[(df["X"] < .03) & (df["Y"] < .03)]
+      if plot:
+        fig = px.line(data_frame=df, x='X', y='Y')
+        fig.update_layout(
+            xaxis=dict(
+                tickmode='linear',
+                dtick=0.0005,
+                range=[0, .005]
+              ),
+            yaxis=dict(
+                tickmode='linear',
+                dtick=0.001,
+                range=[0, .026]
+              ),
+            template='plotly_dark'
+            )
+        
+        if lines:
+          for i, x in enumerate(lines):
+            fig.add_trace(go.Scatter(x=x['X'], y=x['Y'], name=f"line{i}", 
+                          marker=dict(color=tuple(random(size=3) * 256) , size=12, 
+                                      line=dict(width=1))))
+            
+        fig.show()
+
+    Graph(True, [line1, line2, line3])
 
   @staticmethod
   def Three():
